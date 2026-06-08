a96141c84a8a8f3e5632ec834e4787e977ecf924
ernscht <ernscht@gmail.com>
2026-06-08 22:19:21 +0200

    feat: add support for stylelint 17 #37

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
 	extends: [
 		'@merkle-open/eslint-config/configurations/es8-node.js',
 	],
+	settings: {
+		'import/core-modules': ['stylelint'],
+	},
 	globals: {
 		testRule: 'readonly',
 	},
